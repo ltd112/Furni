abdb63b54af81e53b9a38298e34377bf5ae6f95c
ltd112 <lethanhdat032755@gmail.com>
2024-09-14 12:27:39 +0700

    config lai security

--- src/main/java/com/dat/furni/exception/ErrorCode.java
+++ src/main/java/com/dat/furni/exception/ErrorCode.java
@@ -4,20 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.sql.results.graph.collection.internal.UnfetchedCollectionAssembler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    USER_NOT_FOUND(404, "User not found"),
-    USER_ALREADY_EXISTS(400, "User already exists"),
-    INVALID_REQUEST(400, "Invalid request"),
-    INTERNAL_SERVER_ERROR(500, "Internal server error"),
-    UNCAUGHT_EXCEPTION(500, "Uncaught exception"),
-    USERNAME_INVALID(400, "Username must be at least 5 characters long"),
-    PASSWORD_INVALID(400, "Password must be at least 8 characters long"),
-    UNAUTHENTICATED(401, "Unauthenticated"),
+    USER_NOT_FOUND(404, "User not found", HttpStatus.NOT_FOUND),
+    USER_ALREADY_EXISTS(400, "User already exists", HttpStatus.BAD_REQUEST),
+    INVALID_REQUEST(400, "Invalid request", HttpStatus.BAD_REQUEST),
+    INTERNAL_SERVER_ERROR(500, "Internal server error", HttpStatus.INTERNAL_SERVER_ERROR),
+    UNCAUGHT_EXCEPTION(500, "Uncaught exception", HttpStatus.INTERNAL_SERVER_ERROR),
+    USERNAME_INVALID(400, "Username must be at least 5 characters long", HttpStatus.BAD_REQUEST),
+    PASSWORD_INVALID(400, "Password must be at least 8 characters long", HttpStatus.BAD_REQUEST),
+    UNAUTHENTICATED(401, "Unauthenticated", HttpStatus.UNAUTHORIZED),
+    UNAUTHORIZED(403, "Unauthorized", HttpStatus.FORBIDDEN)
     ;
 
     private final int code;
     private final String message;
+    private final HttpStatusCode statusCode;
 }
--- src/main/java/com/dat/furni/service/impl/UserServiceImpl.java
+++ src/main/java/com/dat/furni/service/impl/UserServiceImpl.java
@@ -16,6 +16,7 @@ import lombok.experimental.FieldDefaults;
 
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -70,5 +71,14 @@ public class UserServiceImpl implements UserService {
     @Override
     public void deleteUser(String userId) {
         userRepository.deleteById(userId);
+    }
+
+    @Override
+    public UserResponse getMyInfo() {
+        var context = SecurityContextHolder.getContext();
+        String name = context.getAuthentication().getName();
+
+        User user = userRepository.findByUsername(name).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+        return userMapper.toUserResponse(user);
     }
 }
